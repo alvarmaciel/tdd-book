67525e3d4b91f5647e115c9b749b5a4f5f530e14
alvarmaciel <alvarmaciel@gmail.com>
2021-11-14 14:35:28 -0300

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -19,29 +19,30 @@ class NewVisitorTest(unittest.TestCase):
 
         # Se notice the page title an header mention to-do list
         self.assertIn("To-Do", self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
+        header_text = self.browser.find_element_by_tag_name("h1").text
+        self.assertIn("To-Do", header_text)
         # She is invited to enter a to-do intem straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(inputbox.get_attribute(
-            'placeholder'), 'Enter a to-do item')
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
 
         # She types "Buy Peacock feathers" into a text box
-        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys("Buy peacock feathers")
 
         # When she hits enter, the page updates, ans now the page list
         # "1: Buy Peacock Feathers" as an item in a to-do list.
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows))
+            any(row.text == "1: Buy peacock feathers" for row in rows),
+            "New to-do item did not appear in table",
+        )
 
         # There is still a text box inviting her to add anothes item. She enters "Use
         # peacock feathers to make a fly"
-        self.fail('test Finalizado')
+        self.fail("test Finalizado")
 
         # The page updates again, and now shows both items in her list
 
